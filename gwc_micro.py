a312ea18142140f5d53875ca56ab5e1cf17a6d7e
icmicm <i_c_m@me.com>
2022-10-29 20:38:59 -0400

    V4.1

--- gwc_micro.py
+++ gwc_micro.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas.core.indexes.numeric import Int64Index
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
 scraper = pd.read_html("https://en.wikipedia.org/wiki/List_of_Olympic_medalists_in_judo")
@@ -15,7 +16,7 @@ scraper[2].loc[2, "Games"] = "1968 Mexico City details"
 
 
 # lowercase input from user
-req = {"sex": "Women", "category": "Lightweight", "country": "Canada"}
+req = {"sex": "Women", "category": None, "country": "Canada"}
 
 # hard coded list of table data
 tables = [["Men", "Extra Lightweight"],
@@ -34,25 +35,85 @@ tables = [["Men", "Extra Lightweight"],
           ["Women", "Half Heavyweight"],
           ["Women", "Heavyweight"]]
 
-
 medals = ["Gold", "Silver", "Bronze"]
 
 
 
-# country request section (need an if statement here based on the input
-fighters = []
+"""
 
-if req["sex"] == "Men":
-    del tables[8:16]
-elif req["sex"] == "Women":
-    del tables[0:8]
-
-if req["category"] is not None:
-    tables = [i for i in tables if i[1] == req["category"]]
 
 #if req["country"] is not None:
+"""
 
 
-print(fighters)
+class Person:
 
-print(scraper)
+    def __init__(self, name, sex, origin):
+        self.name = name
+        self.sex = sex
+        self.origin = origin
+        self.gold = []
+        self.silver = []
+        self.bronze = []
+
+    def addMedals(self, gold, silver, bronze):
+        self.gold.append(gold)
+        self.silver.append(silver)
+        self.bronze.append(bronze)
+
+
+fighters = [Person("ian", "Men", "canada")]
+
+if req["sex"] == "Men":
+    start = 0
+    end = 8
+elif req["sex"] == "Women":
+    start = 8
+    end = 15
+else:
+    start = 0
+    end = 15
+
+cat_tables = []
+if req["category"] is not None:
+    for i in range(len(tables)):
+        if tables[i][1] == req["category"]:
+            cat_tables.append(i)
+
+for table in range(start, end):
+    if table not in cat_tables and req["category"] is not None:
+        continue
+    sex = tables[table][0]
+    category = tables[table][1]
+    for row in range(len(scraper[table])):
+        game = scraper[table]["Games"][row].replace(" details", "")
+        for medal in medals:
+            cell = scraper[table][medal][row].split("\xa0")
+            name = cell[0]
+            if name == "not included in the Olympic program":
+                continue
+            elif name == "none awarded":
+                continue
+            else:
+                origin = cell[1]
+                flag = False
+                for fighter in fighters:
+                    if name == fighter.name:
+                        flag = True
+                        break
+                if flag is False:
+                    new_person = Person(name, sex, origin)
+                    fighters.append(new_person)
+                for fighter in fighters:
+                    if name == fighter.name:
+                        if medal == "Gold":
+                            fighter.addMedals(game + " - " + category, None, None)
+                        elif medal == "Silver":
+                            fighter.addMedals(None, game + " - " + category, None)
+                        else:
+                            fighter.addMedals(None, None, game + " - " + category)
+
+
+new_fighters = [x for x in fighters if x.origin == req["country"]]
+del new_fighters[0]
+print(new_fighters)
